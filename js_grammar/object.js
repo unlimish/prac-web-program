7558fe3cb7c7fcecf2161dceda6b07d94678ecda
unlimish <14168376+unlimish@users.noreply.github.com>
2020-07-01 12:00:17 +0900

    Refactor: stop()

--- js_grammar/object.js
+++ js_grammar/object.js
@@ -1,10 +1,12 @@
 var startTime = null;
 function start() {
     startTime = Date.now();
-    document.body.onkeydown = function() {
-        console.log('Stopped');
-    }
+    document.body.onkeydown = stop;
     console.log('Started');
+}
+
+function stop() {
+    console.log('Stopped')
 }
 
 if (confirm('Press ok then if 10 sec passed, press any key.')) {
