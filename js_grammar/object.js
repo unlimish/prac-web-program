bc833c6f7cf161d52e27cf254e12d1dcbc360337
unlimish <14168376+unlimish@users.noreply.github.com>
2020-07-01 14:06:05 +0900

    🔧Fix function names by merged one object: game

--- js_grammar/object.js
+++ js_grammar/object.js
@@ -3,20 +3,20 @@ var game = {
     displayArea: document.getElementById('display-area'),
     start: function () {
         startTime = Date.now();
-        document.body.onkeydown = stop;
+        document.body.onkeydown = game.stop;
     },
     stop: function () {
         var currentTime = Date.now();
-        var seconds = (currentTime - startTime) / 1000;
+        var seconds = (currentTime - game.startTime) / 1000;
 
         if (9.5 <= seconds && seconds <= 10.5) {
-            displayArea.innerText = 'Wow, you pressed in' + seconds + 'seconds!';
+            game.displayArea.innerText = 'Wow, you pressed in' + seconds + 'seconds!';
         } else {
-            displayArea.innerText = 'Oops, you pressed in' + seconds + 'seconds.'
+            game.displayArea.innerText = 'Oops, you pressed in' + seconds + 'seconds.'
         }
     }
 }
 
 if (confirm('Press ok then if 10 sec passed, press any key.')) {
-    start();
+    game.start();
 }
