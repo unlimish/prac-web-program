25206285157b17dd55351c1928245847a682ba96
unlimish <14168376+unlimish@users.noreply.github.com>
2020-07-01 12:08:35 +0900

    Add method: judgement by pressed time

--- js_grammar/object.js
+++ js_grammar/object.js
@@ -1,12 +1,18 @@
 var startTime = null;
+var displayArea = document.getElementById('display-area');
 function start() {
     startTime = Date.now();
     document.body.onkeydown = stop;
-    console.log('Started');
 }
 
 function stop() {
-    console.log('Stopped')
+    var currentTime = Date.now();
+    var seconds = (currentTime - startTime) / 1000;
+    if (9.5 <= seconds && seconds <= 10.5) {
+        displayArea.innerText = 'Wow, you pressed in' + seconds + 'seconds!';
+    } else {
+        displayArea.innerText = 'Oops, you pressed in' + seconds + 'seconds.'
+    }
 }
 
 if (confirm('Press ok then if 10 sec passed, press any key.')) {
